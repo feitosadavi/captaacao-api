b37ba40d6153c0224f251e0b6f906be5ba034f68
feitosadavi <davifeitosa.dev@protonmail.com>
2021-09-19 13:36:27 -0300

    test: change file name and add /accounts test

---
+++ src/main/routes/account-routes.test.ts
@@ -0,0 +1,104 @@
+import { Collection, InsertOneWriteOpResult } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+import { hash } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import env from '../config/env'
+
+let accountsCollection: Collection
+
+const insertAccount = async (): Promise<InsertOneWriteOpResult<any>> => {
+  return accountsCollection.insertOne({
+    name: 'Carlos',
+    email: 'carlos@gmail.com',
+    password: '123',
+    role: 'admin'
+  })
+}
+
+const updateAccountToken = async (id: string, accessToken: string): Promise<void> => {
+  await accountsCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+}
+
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
+  })
+
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Davi',
+          email: 'davifeitosa.dev@protonmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('POST /login', () => {
+    test('Should return 200 on login', async () => {
+      const password = await hash('123', 12)
+      await accountsCollection.insertOne({
+        name: 'Davi',
+        email: 'davifeitosa.dev@protonmail.com',
+        password
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'davifeitosa.dev@protonmail.com',
+          password: '123'
+        })
+        .expect(200)
+    })
+
+    test('Should return 401 if login is unauthorized', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'davifeitosa.dev@gmail.com',
+          password: '123456'
+        })
+        .expect(401)
+    })
+  })
+
+  describe('GET /accounts', () => {
+    test('should return 403 if user has no authorization', async () => {
+      await request(app)
+        .get('/api/accounts')
+        .expect(403)
+    })
+    test('should return 200 on success', async () => {
+      const res = await insertAccount()
+      const id = res.ops[0]._id
+      const accessToken = sign({ id }, env.secret)
+      await updateAccountToken(id, accessToken)
+
+      await request(app)
+        .get('/api/accounts')
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
+  })
+})
