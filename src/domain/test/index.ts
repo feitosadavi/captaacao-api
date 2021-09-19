20f58b92435a2cc8e8972c80eeef9a87b3c0e210
feitosadavi <davifeitosa.dev@protonmail.com>
2021-09-19 16:28:15 -0300

    refactor: remove duplicated export

--- src/domain/test/index.ts
+++ src/domain/test/index.ts
@@ -3,7 +3,6 @@ export * from './test-helpers'
 export * from './mock-car'
 export * from './mock-hasher'
 export * from './mock-encrypter'
-export * from './mock-encrypter'
 export * from './mock-log-decorator'
 export * from './mock-validation'
 
