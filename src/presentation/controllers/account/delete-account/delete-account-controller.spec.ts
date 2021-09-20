a20cdeea69b1f15340a0a5ae28d2fa32de501f99
feitosadavi <davifeitosa.dev@protonmail.com>
2021-09-20 19:10:03 -0300

    test: ensure DeleteAccountController returns 401 if user is not admin or is not deleting your own account

---
+++ src/presentation/controllers/account/delete-account/delete-account-controller.spec.ts
@@ -0,0 +1,46 @@
+import { mockAccountModel, mockDeleteAccount, mockLoadAccountByToken } from '@/domain/test'
+import { DeleteAccount } from '@/domain/usecases/account/delete-account'
+import { unauthorized } from '@/presentation/helpers/http/http-helper'
+import { LoadAccountByToken } from '@/presentation/middlewares/auth-middleware-protocols'
+import { DeleteAccountController } from './delete-account-controller'
+
+type SutTypes = {
+  sut: DeleteAccountController
+  deleteAccountStub: DeleteAccount
+  loadAccountByTokenStub: LoadAccountByToken
+}
+
+const makeSut = (): SutTypes => {
+  const deleteAccountStub = mockDeleteAccount()
+  const loadAccountByTokenStub = mockLoadAccountByToken()
+  const sut = new DeleteAccountController(deleteAccountStub, loadAccountByTokenStub)
+  return {
+    sut,
+    deleteAccountStub,
+    loadAccountByTokenStub
+  }
+}
+
+// id: 'any_id',
+// name: 'any_name',
+// email: 'any_email@mail.com',
+// password: 'hashed_password',
+// cpf: 'any_cpf',
+// birthDate: '00/00/0000',
+// phoneNumber: '9999999999999',
+// role: 'any_role'
+
+describe('DeleteAccount Controller', () => {
+  test('Should return 401 if user is not admin or is not deleting your own account', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    const loadAccountByTokenStubSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+    loadAccountByTokenStubSpy.mockReturnValueOnce(Promise.resolve({ ...mockAccountModel() }))
+    const response = await sut.handle({
+      params: { id: 'other_id' },
+      headers: {
+        'x-access-token': 'any_access_token'
+      }
+    })
+    expect(response).toEqual(unauthorized())
+  })
+})
