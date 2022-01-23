a8c4ba68c518f1de49e5a54f8e8181ed755e8eb6
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-23 12:24:00 -0300

    chore: change some jest configs

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,13 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/**/*-protocols.ts',
-    '!<rootDir>/src/**/protocols/*',
+    // '!<rootDir>/src/**/*-protocols.ts',
+    // '!<rootDir>/src/**/protocols/*',
     '!<rootDir>/src/main/**'
   ],
   coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
