3d3aabc2b9e4ea1b2672c3156a412fc43c47cd91
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-25 20:34:04 -0300

    feat: add UpdateAccount domain usecase

---
+++ src/domain/usecases/account/update-account.ts
@@ -0,0 +1,11 @@
+export interface UpdateAccount {
+  update (params: UpdateAccount.Params): Promise<UpdateAccount.Result>
+}
+
+export namespace UpdateAccount {
+  export type Params = {
+    id: string
+    fields: [Record<string, any>]
+  }
+  export type Result = boolean
+}
