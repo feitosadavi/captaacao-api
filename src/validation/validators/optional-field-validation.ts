b03d42232aa71865bd56860ea96a0ca33f775133
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-26 13:43:17 -0300

    test: should return InvalidParamError if validation with only nested params fails

--- src/validation/validators/optional-field-validation.ts
+++ src/validation/validators/optional-field-validation.ts
@@ -1,13 +1,31 @@
 import { InvalidParamError } from '@/presentation/errors'
 import { Validation } from '@/presentation/protocols/validation'
 
+// TRABALHE DA IDEIA DA VALIDAÇÃO DE SCHEMA!!!!
+
 export class OptionalFieldValidation implements Validation {
-  constructor (private readonly options: string[]) { }
+  constructor (private readonly options: string[], private readonly nestedFields?: string[]) { }
 
   validate (input: any): Error {
-    const inputField = Object.keys(input)[0]
-    if (!this.options.includes(inputField)) {
-      return new InvalidParamError(inputField)
+    const fields = Object.keys(input)
+    for (const field of fields) {
+      if (!this.options.includes(field)) {
+        return new InvalidParamError(field)
+      } else {
+        const hasKeys = !!Object.keys(input[field]).length
+        if (this.nestedFields?.length > 0 && hasKeys) {
+          const nestedKeys = Object.keys(input[field])
+          for (const key of nestedKeys) {
+            if (!this.nestedFields.includes(input[field][key])) {
+              return new InvalidParamError(key)
+            }
+          }
+        }
+      }
     }
   }
 }
+
+// if (!this.options.includes(input[field])) {
+//   return new InvalidParamError(field)
+// }
--- tests/validation/usecases/optional-field-validation.spec.ts
+++ tests/validation/usecases/optional-field-validation.spec.ts
@@ -6,8 +6,8 @@ type SutTypes = {
   sut: Validation
 }
 
-const makeSut = (options: string[]): SutTypes => {
-  const sut = new OptionalFieldValidation(options)
+const makeSut = (options: string[], nestedFields?: string[]): SutTypes => {
+  const sut = new OptionalFieldValidation(options, nestedFields)
   return {
     sut
   }
@@ -26,5 +26,13 @@ describe('OptionalFieldValidation', () => {
     const { sut } = makeSut(options)
     const error = sut.validate({ field: 'any_field' })
     expect(error).toBeFalsy()
+  })
+
+  test('Should OptionalFieldValidation returns InvalidParamError if validation with only nested params fails', () => {
+    const options = ['person']
+    const nestedOptions = ['nested_field']
+    const { sut } = makeSut(options, nestedOptions)
+    const error = sut.validate({ person: { name: 'any_name', adress: 'any_adress' } })
+    expect(error).toEqual(new InvalidParamError('name'))
   })
 })
