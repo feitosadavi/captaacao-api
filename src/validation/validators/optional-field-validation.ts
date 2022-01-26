cac817654ad408b932875fbbdb4594d94750a2b1
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-26 14:25:26 -0300

    refactor: remove console logs

--- src/validation/validators/optional-field-validation.ts
+++ src/validation/validators/optional-field-validation.ts
@@ -4,23 +4,19 @@ import { Validation } from '@/presentation/protocols/validation'
 // TRABALHE DA IDEIA DA VALIDAÇÃO DE SCHEMA!!!!
 
 export class OptionalFieldValidation implements Validation {
-  constructor (private readonly options: string[], private readonly nestedFields?: string[]) { }
+  constructor (private readonly permittedFields: string[], private readonly permittedNestedFields?: string[]) { }
 
   validate (input: any): Error {
     const fields = Object.keys(input)
     for (const field of fields) {
-      if (!this.options.includes(field)) {
+      if (!this.permittedFields.includes(field)) {
         return new InvalidParamError(field)
       } else {
         const hasKeys = !!Object.keys(input[field]).length
-        if (this.nestedFields?.length > 0 && hasKeys) {
-          const nestedKeys = Object.keys(input[field])
-          console.log('this nestedFields: ', this.nestedFields)
-          for (const key of nestedKeys) {
-            console.log({ key })
-            // console.log('obj key: ', Object.keys(input[field][key])[0])
-            console.log(!this.nestedFields.includes(key))
-            if (!this.nestedFields.includes(key)) {
+        if (this.permittedNestedFields?.length > 0 && hasKeys) {
+          const existentNestedKeys = Object.keys(input[field])
+          for (const key of existentNestedKeys) {
+            if (!this.permittedNestedFields.includes(key)) {
               return new InvalidParamError(key)
             }
           }
@@ -30,6 +26,6 @@ export class OptionalFieldValidation implements Validation {
   }
 }
 
-// if (!this.options.includes(input[field])) {
+// if (!this.permittedFields.includes(input[field])) {
 //   return new InvalidParamError(field)
 // }
