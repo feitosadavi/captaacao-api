d40ef624e91edb5b55bd7082ff889730f1fa627b
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-26 19:43:41 -0300

    test: should UpdateAccountValidation Factory call ValidationComposite with all validations

---
+++ tests/main/factories/update-account-validation-factory.spec.ts
@@ -0,0 +1,39 @@
+import { OptionalFieldValidation, ValidationComposite } from '@/validation/validators'
+import { Validation } from '@/presentation/protocols/validation'
+import { makeUpdateAccountValidationFactory } from '@/main/factories'
+
+// quando eu mocko um módulo, ele passa a não ter mais o comportamento default dele
+jest.mock('@/validation/validators/validation-composite')
+
+// aqui vamos garantir que o validation composite não irá deixar de injetar nenhuma validação que precisa ter
+describe('SignUpValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeUpdateAccountValidationFactory()
+    const validations: Validation[] = []
+    const allowedFields = ['name',
+      'profileType',
+      'profilePhoto',
+      'doc',
+      'birthDate',
+      'password',
+      'email',
+      'phone',
+      'role',
+      'adress'
+    ]
+    const adressOptions = [
+      'cep',
+      'endereco',
+      'complemento',
+      'uf',
+      'cidade',
+      'bairro'
+    ]
+    const notificationFields = ['message', 'createdAt', 'isSeen']
+    const ratingFields = ['message', 'createdAt', 'status', 'rater']
+    const allowedNestedFields = [...adressOptions, ...notificationFields, ...ratingFields]
+
+    validations.push(new OptionalFieldValidation(allowedFields, allowedNestedFields))
+    expect(ValidationComposite).toHaveBeenLastCalledWith(validations)
+  })
+})
