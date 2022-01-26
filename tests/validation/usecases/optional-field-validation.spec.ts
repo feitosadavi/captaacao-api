bcb16c471204763521740cc0121b7d0b4769575f
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-26 14:23:33 -0300

    test: should return nothing if validation succeed

--- tests/validation/usecases/optional-field-validation.spec.ts
+++ tests/validation/usecases/optional-field-validation.spec.ts
@@ -6,8 +6,8 @@ type SutTypes = {
   sut: Validation
 }
 
-const makeSut = (options: string[], nestedFields?: string[]): SutTypes => {
-  const sut = new OptionalFieldValidation(options, nestedFields)
+const makeSut = (permittedFields: string[], permittedNestedFields?: string[]): SutTypes => {
+  const sut = new OptionalFieldValidation(permittedFields, permittedNestedFields)
   return {
     sut
   }
@@ -15,24 +15,32 @@ const makeSut = (options: string[], nestedFields?: string[]): SutTypes => {
 
 describe('OptionalFieldValidation', () => {
   test('Should OptionalFieldValidation returns InvalidParamError if validation fails', () => {
-    const options = ['field', 'other_field']
-    const { sut } = makeSut(options)
+    const permittedFields = ['field', 'other_field']
+    const { sut } = makeSut(permittedFields)
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new InvalidParamError('name'))
   })
 
   test('Should OptionalFieldValidation returns nothing if validation succeed', () => {
-    const options = ['field', 'other_field']
-    const { sut } = makeSut(options)
+    const permittedFields = ['field', 'other_field']
+    const { sut } = makeSut(permittedFields)
     const error = sut.validate({ field: 'any_field' })
     expect(error).toBeFalsy()
   })
 
   test('Should OptionalFieldValidation returns InvalidParamError if validation with only nested params fails', () => {
-    const options = ['person']
-    const nestedOptions = ['nested_field']
-    const { sut } = makeSut(options, nestedOptions)
+    const permittedFields = ['person']
+    const permittedNestedFields = ['nested_field']
+    const { sut } = makeSut(permittedFields, permittedNestedFields)
     const error = sut.validate({ person: { name: 'any_name', adress: 'any_adress' } })
     expect(error).toEqual(new InvalidParamError('name'))
+  })
+
+  test('Should OptionalFieldValidation returns nothing if validation succeed', () => {
+    const permittedFields = ['field']
+    const permittedNestedFields = ['nested_field', 'other_nested_field']
+    const { sut } = makeSut(permittedFields, permittedNestedFields)
+    const error = sut.validate({ field: { nested_field: 'any_value', other_nested_field: 'other_value' } })
+    expect(error).toBeFalsy()
   })
 })
