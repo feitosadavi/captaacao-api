ff1048f8062a006e76169ec4da32601be129ac4d
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-26 01:58:58 -0300

    test: create UpdateAccount controller

---
+++ tests/presentation/usecases/controllers/account/update-account-controller.spec.ts
@@ -0,0 +1,65 @@
+import { UpdateAccount } from '@/domain/usecases'
+import { UpdateAccountController } from '@/presentation/controllers'
+import { serverSuccess, badRequest, serverError } from '@/presentation/helpers/http/http-helper'
+import { HttpRequest, Validation } from '@/presentation/protocols'
+import { throwError } from '@tests/domain/mocks'
+import { mockUpdateAccount, mockValidation } from '@tests/presentation/mocks'
+
+type SutTypes = {
+  sut: UpdateAccountController
+  validationStub: Validation
+  updateAccountStub: UpdateAccount
+}
+const makeSut = (): SutTypes => {
+  const updateAccountStub = mockUpdateAccount()
+  const validationStub = mockValidation()
+  const sut = new UpdateAccountController(validationStub, updateAccountStub)
+  return {
+    sut,
+    validationStub,
+    updateAccountStub
+  }
+}
+const mockRequest = (): HttpRequest => ({
+  body: {
+    email: 'other_email@mail.com',
+    name: 'other_name'
+  }
+})
+describe('UpdateAccount Controller', () => {
+  test('Should return 400 if Validation returns an error ', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+
+  test('Should call updateAccount with correct values', async () => {
+    const { sut, updateAccountStub } = makeSut()
+    const request = mockRequest()
+    const updateSpy = jest.spyOn(updateAccountStub, 'update')
+    await sut.handle(request)
+    expect(updateSpy).toHaveBeenCalledWith(request.body)
+  })
+
+  test('Should call updateAccount with correct values', async () => {
+    const { sut, updateAccountStub } = makeSut()
+    const request = mockRequest()
+    jest.spyOn(updateAccountStub, 'update').mockImplementationOnce(throwError)
+    const httpResponse = await sut.handle(request)
+    expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 500 if Validation throws', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverSuccess({ ok: true }))
+  })
+
+  test('Should return 200 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(serverSuccess({ ok: true }))
+  })
+})
