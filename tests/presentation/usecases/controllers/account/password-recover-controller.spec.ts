5a9a6d058a908494b7d048481580fb0b812b165f
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-29 17:11:20 -0300

    test: should PasswordRecover returns 400 if LoadAccountById do not find any account

--- tests/presentation/usecases/controllers/account/password-recover-controller.spec.ts
+++ tests/presentation/usecases/controllers/account/password-recover-controller.spec.ts
@@ -1,5 +1,7 @@
 import { LoadAccountById } from '@/domain/usecases/account/load-account-by-id'
 import { PasswordRecoverController } from '@/presentation/controllers/account/password-recover-controller'
+import { NotFoundAccountError } from '@/presentation/errors/not-found-account'
+import { badRequest } from '@/presentation/helpers/http/http-helper'
 import { HttpRequest } from '@/presentation/protocols'
 
 import { mockLoadAccountById } from '@tests/presentation/mocks'
@@ -32,5 +34,12 @@ describe('PasswordRecover Controller', () => {
     const loadAccountByIdSpy = jest.spyOn(LoadAccountByIdStub, 'loadById')
     await sut.handle(mockRequest())
     expect(loadAccountByIdSpy).toHaveBeenCalledWith('any_id')
+  })
+
+  test('Should return 400 if LoadAccountById dont find any account', async () => {
+    const { sut, LoadAccountByIdStub } = makeSut()
+    jest.spyOn(LoadAccountByIdStub, 'loadById').mockReturnValueOnce(Promise.resolve(null))
+    const httpResponse = await sut.handle(mockRequest())
+    expect(httpResponse).toEqual(badRequest(new NotFoundAccountError()))
   })
 })
