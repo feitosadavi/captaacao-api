e83d4b68fbcaccadf28d307f1f020c2a267d4797
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-29 20:16:57 -0300

    feat: should PasswordRecover Controller return 500 if any depedencies throw

--- src/presentation/controllers/account/password-recover-controller.ts
+++ src/presentation/controllers/account/password-recover-controller.ts
@@ -1,7 +1,7 @@
 import { GeneratePassRecoverInfo } from '@/data/protocols/others'
 import { LoadIdByEmail, UpdateAccount } from '@/domain/usecases'
 import { NotFoundAccountError } from '@/presentation/errors/not-found-account'
-import { badRequest, serverSuccess } from '@/presentation/helpers/http/http-helper'
+import { badRequest, serverError, serverSuccess } from '@/presentation/helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
 
 export class PasswordRecoverController implements Controller {
@@ -11,12 +11,16 @@ export class PasswordRecoverController implements Controller {
     private readonly generatePassRecoverInfo: GeneratePassRecoverInfo
   ) { }
 
-  async handle (httpRequest: HttpRequest<any, any, {id: string}>): Promise<HttpResponse> {
-    const accountId = httpRequest.params.id
-    const account = await this.loadIdByEmail.load({ id: accountId })
-    if (!account) return badRequest(new NotFoundAccountError())
-    const recoverPassInfo = this.generatePassRecoverInfo.generate()
-    const isUpdated = await this.updateAccount.update({ id: accountId, fields: { recoverPassInfo } })
-    return isUpdated && serverSuccess({ ok: true })
+  async handle (httpRequest: HttpRequest<any, any, { id: string }>): Promise<HttpResponse> {
+    try {
+      const accountId = httpRequest.params.id
+      const account = await this.loadIdByEmail.load({ id: accountId })
+      if (!account) return badRequest(new NotFoundAccountError())
+      const recoverPassInfo = this.generatePassRecoverInfo.generate()
+      const isUpdated = await this.updateAccount.update({ id: accountId, fields: { recoverPassInfo } })
+      return isUpdated && serverSuccess({ ok: true })
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
