b278b90f96f0b16a51bd71ca65352cef6b61184b
feitosadavi <davifeitosa.dev@protonmail.com>
2022-02-01 16:48:22 -0300

    feat: should /password-recover returns 200 on success

--- src/main/routes/account-routes.ts
+++ src/main/routes/account-routes.ts
@@ -8,15 +8,20 @@ import {
   makeLoginController,
   makeSignUpController,
   makeAuthMiddleware,
-  makeUpdateAccountControllerFactory
+  makeUpdateAccountControllerFactory,
+  makePasswordRecoverController
 } from '@/main/factories'
 import { adminAuth, auth } from '@/main/middlewares'
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignUpController()))
   router.post('/login', adaptRoute(makeLoginController()))
+  router.post('/account/password-recover', adaptRoute(makePasswordRecoverController()))
+
   router.get('/accounts', adminAuth, adaptRoute(makeLoadAccountsController()))
   router.get('/accounts/:id', adminAuth, adaptRoute(makeLoadAccountsController()))
+
   router.delete('/accounts/delete/:id', adaptMiddleware(makeAuthMiddleware(null, true)), adaptRoute(makeDeleteAccountsController()))
+
   router.put('/account/update', auth, adaptRoute(makeUpdateAccountControllerFactory()))
 }
