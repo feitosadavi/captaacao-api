1e5651fb9150b0c80658f42b0fde6ec7d2482717
feitosadavi <davifeitosa.dev@protonmail.com>
2022-01-31 23:34:27 -0300

    feat: create validation factory

--- src/main/factories/controllers/account/password-recover-validation-factory.ts
+++ src/main/factories/controllers/account/password-recover-validation-factory.ts
@@ -1,9 +1,13 @@
-import { ValidationComposite, EmailValidation } from '@/validation/validators'
+import { ValidationComposite, EmailValidation, RequiredFieldValidation } from '@/validation/validators'
 import { Validation } from '@/presentation/protocols/validation'
 import { EmailValidatorAdapter } from '@/infra/validators'
 
 export const makePasswordRecoverValidation = (): ValidationComposite => {
   const validations: Validation[] = []
+  for (const field of ['email']) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+
   validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
   return new ValidationComposite(validations)
 }
