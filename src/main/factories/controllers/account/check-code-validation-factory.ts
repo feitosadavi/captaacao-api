141cb0ef42947948ccf590dfced178c383835520
feitosadavi <davifeitosa.dev@protonmail.com>
2022-02-09 17:54:30 -0300

    feat: add check code validation factory

---
+++ src/main/factories/controllers/account/check-code-validation-factory.ts
@@ -0,0 +1,11 @@
+import { ValidationComposite } from '@/validation/validators'
+import { Validation } from '@/presentation/protocols/validation'
+import { SixDigitCodeValidation } from '@/presentation/validators'
+
+// este factory foi criado para que possamos testar apenas o validation de forma isolada
+export const makeCheckCodeValidatin = (): ValidationComposite => {
+  // para cada campo que eu tiver, vou adicionar uma validação
+  const validations: Validation[] = []
+  validations.push(new SixDigitCodeValidation())
+  return new ValidationComposite(validations)
+}
