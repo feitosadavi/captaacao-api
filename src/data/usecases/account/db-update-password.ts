3c7e4e10662cc2308bf31ab1c0606f81b9ead390
feitosadavi <davifeitosa.dev@protonmail.com>
2022-02-14 17:08:43 -0300

    feat: should call Hasher with correct params

--- src/data/usecases/account/db-update-password.ts
+++ src/data/usecases/account/db-update-password.ts
@@ -1,17 +1,19 @@
-import { LoadAccountByIdRepository, UpdatePasswordRepository } from '@/data/protocols'
+import { Hasher, LoadAccountByIdRepository, UpdatePasswordRepository } from '@/data/protocols'
 import { UpdatePassword } from '@/domain/usecases'
 
 export class DbUpdatePassword implements UpdatePassword {
   constructor (
+    private readonly hasher: Hasher,
     private readonly updatePasswordRepository: UpdatePasswordRepository,
     private readonly loadAccountByIdRepository: LoadAccountByIdRepository
   ) { }
 
-  async update (params: UpdatePassword.Params): Promise<boolean> {
-    const account = await this.loadAccountByIdRepository.loadById(params.id)
+  async update ({ id, password }: UpdatePassword.Params): Promise<boolean> {
+    const hashedPassword = await this.hasher.hash(password)
+    const account = await this.loadAccountByIdRepository.loadById(id)
     let result = false
     if (account?.id) {
-      result = await this.updatePasswordRepository.updatePassword(params)
+      result = await this.updatePasswordRepository.updatePassword({ password: hashedPassword, id })
     }
     return result
   }
