8b5b51305026b189004d795c6b248f86c59c4fc6
feitosadavi <davifeitosa.dev@protonmail.com>
2022-02-17 15:04:20 -0300

    refactor: move profile params mock to mock-profile file

--- tests/domain/mocks/mock-profile.ts
+++ tests/domain/mocks/mock-profile.ts
@@ -1,4 +1,9 @@
 import { ProfileModel } from '@/domain/models'
+import { AddProfile } from '@/domain/usecases'
+
+export const mockProfileParams = (): AddProfile.Params => ({
+  name: 'any_name'
+})
 
 export const mockProfileModel = (): ProfileModel => ({
   id: 'any_id',
