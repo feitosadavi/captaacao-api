fbaaceff200b9c6e65f80fa42b42cbef5e6cc784
feitosadavi <davifeitosa.dev@protonmail.com>
2022-02-17 15:32:54 -0300

    feat: should CheckProfileBynameRepositoy returns true if profile exists

--- src/infra/db/mongodb/profile-mongo-repository.ts
+++ src/infra/db/mongodb/profile-mongo-repository.ts
@@ -1,10 +1,17 @@
-import { AddProfileRepository } from '@/data/protocols'
+import { AddProfileRepository, CheckProfileByNameRepository } from '@/data/protocols'
 import { MongoHelper } from '.'
 
-export class ProfileMongoRepository implements AddProfileRepository {
+export class ProfileMongoRepository implements AddProfileRepository, CheckProfileByNameRepository {
   async add (params: AddProfileRepository.Params): Promise<AddProfileRepository.Result> {
     const profilesCollection = await MongoHelper.getCollection('profiles')
     const result = await profilesCollection.insertOne(params)
     return !!result.insertedId
+  }
+
+  // trocar por name is in use
+  async checkByName ({ name }: CheckProfileByNameRepository.Params): Promise<CheckProfileByNameRepository.Result> {
+    const profilesCollection = await MongoHelper.getCollection('profiles')
+    const profile = await profilesCollection.findOne({ name })
+    return !!profile._id
   }
 }
