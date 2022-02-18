f5cbe28e4aec31220c87c9a64396029d1e091d17
feitosadavi <davifeitosa.dev@protonmail.com>
2022-02-18 18:04:24 -0300

    feat: add LoadProfilessRepository to ProfileMongoRepository

--- src/infra/db/mongodb/profile-mongo-repository.ts
+++ src/infra/db/mongodb/profile-mongo-repository.ts
@@ -1,23 +1,28 @@
-import { AddProfileRepository, DeleteProfileRepository, ProfileNameIsInUseRepository } from '@/data/protocols'
+import { AddProfileRepository, DeleteProfileRepository, LoadProfilesRepository, ProfileNameIsInUseRepository } from '@/data/protocols'
 import { MongoHelper } from '.'
 
-export class ProfileMongoRepository implements AddProfileRepository, ProfileNameIsInUseRepository, DeleteProfileRepository {
+export class ProfileMongoRepository implements AddProfileRepository, ProfileNameIsInUseRepository, DeleteProfileRepository, LoadProfilesRepository {
   async add (params: AddProfileRepository.Params): Promise<AddProfileRepository.Result> {
     const profilesCollection = await MongoHelper.getCollection('profiles')
     const result = await profilesCollection.insertOne(params)
     return !!result.insertedId
   }
 
-  // trocar por name is in use
+  async loadProfiles (): Promise<LoadProfilesRepository.Result> {
+    const profilesCollection = await MongoHelper.getCollection('profiles')
+    const profiles = await profilesCollection.find({}).toArray()
+    return profiles && MongoHelper.mapCollection(profiles)
+  }
+
+  async deleteProfile ({ id }: DeleteProfileRepository.Params): Promise<DeleteProfileRepository.Result> {
+    const profilesCollection = await MongoHelper.getCollection('profiles')
+    const deletionResult = await profilesCollection.deleteOne({ _id: id })
+    return deletionResult.deletedCount === 1
+  }
+
   async nameIsInUse ({ name }: ProfileNameIsInUseRepository.Params): Promise<ProfileNameIsInUseRepository.Result> {
     const profilesCollection = await MongoHelper.getCollection('profiles')
     const profile = await profilesCollection.findOne({ name })
     return !!profile._id
-  }
-
-  async deleteProfile ({ id }: DeleteProfileRepository.Params): Promise<DeleteProfileRepository.Result> {
-    const profileCollection = await MongoHelper.getCollection('profiles')
-    const deletionResult = await profileCollection.deleteOne({ _id: id })
-    return deletionResult.deletedCount === 1
   }
 }
