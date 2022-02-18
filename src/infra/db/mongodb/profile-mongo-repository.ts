2bfbd50228d904a66b6407d4060eb24c7188ee61
feitosadavi <davifeitosa.dev@protonmail.com>
2022-02-17 22:23:23 -0300

    feat: add DeleteProfileRepository to ProfileMongoRepository

--- src/infra/db/mongodb/profile-mongo-repository.ts
+++ src/infra/db/mongodb/profile-mongo-repository.ts
@@ -1,7 +1,7 @@
-import { AddProfileRepository, ProfileNameIsInUseRepository } from '@/data/protocols'
+import { AddProfileRepository, DeleteProfileRepository, ProfileNameIsInUseRepository } from '@/data/protocols'
 import { MongoHelper } from '.'
 
-export class ProfileMongoRepository implements AddProfileRepository, ProfileNameIsInUseRepository {
+export class ProfileMongoRepository implements AddProfileRepository, ProfileNameIsInUseRepository, DeleteProfileRepository {
   async add (params: AddProfileRepository.Params): Promise<AddProfileRepository.Result> {
     const profilesCollection = await MongoHelper.getCollection('profiles')
     const result = await profilesCollection.insertOne(params)
@@ -13,5 +13,11 @@ export class ProfileMongoRepository implements AddProfileRepository, ProfileName
     const profilesCollection = await MongoHelper.getCollection('profiles')
     const profile = await profilesCollection.findOne({ name })
     return !!profile._id
+  }
+
+  async deleteProfile ({ id }: DeleteProfileRepository.Params): Promise<DeleteProfileRepository.Result> {
+    const profileCollection = await MongoHelper.getCollection('profiles')
+    const deletionResult = await profileCollection.deleteOne({ _id: id })
+    return deletionResult.deletedCount === 1
   }
 }
