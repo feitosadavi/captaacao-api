547a77c509dfdb6140585986d24051ab30072342
feitosadavi <davifeitosa.dev@protonmail.com>
2022-02-28 20:00:01 -0300

    refactor: change params and return type from query and mutation

--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -6,11 +6,66 @@ export default gql`
   }
 
   extend type Mutation {
-    signUp (name: String!, email: String!, password: String!, passwordConfirmation: String!): Account!
+    signUp (
+      name: String!
+      password: String!
+      passwordConfirmation: String!
+      profileType: String!
+      profilePhoto: String!
+      doc: String!
+      birthDate: String!
+      email: String!
+      phone: String!
+      role: String!
+
+      # Adress
+      cep: String!
+      endereco: String!
+      complemento: String!
+      uf: String!
+      cidade: String!
+      bairro: String!
+    ): Account!
   }
 
   type Account {
     accessToken: String!
     name: String!
   }
+
+  # type Account {
+  #   id: String
+  #   name: String
+  #   profileType: String
+  #   profilePhoto: String
+  #   doc: String
+  #   birthDate: String
+  #   email: String
+  #   phone: String
+  #   role: String
+  #   adress: Adress
+
+  #   accessToken: String
+  #   code: Code
+  #   canUseCookies: Boolean
+  #   status: Boolean
+  #   timeout: Int
+  #   profileViews: Int
+  #   online: Boolean
+  # }
+
+  # type Adress {
+  #   cep: String
+  #   endereco: String
+  #   complemento: String
+  #   uf: String
+  #   cidade: String
+  #   bairro: String
+  # }
+
+  # type Code {
+  #   number: Int
+  #   createdAt: DateTime
+  #   expiresAt: DateTime
+  # }
 `
