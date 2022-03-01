fed5c5dc3417e8ef2145db5e761f8a5405370883
feitosadavi <davifeitosa.dev@protonmail.com>
2022-03-01 18:00:01 -0300

    fix: fix wrong mock

--- tests/domain/mocks/mock-profile.ts
+++ tests/domain/mocks/mock-profile.ts
@@ -11,19 +11,11 @@ export const mockProfileModels = (): ProfileModel[] => ([
   {
     id: 'any_id',
     name: 'any_name',
-    accounts: [
-      'any_account_id',
-      'other_account_id'
-    ],
     createdAt: new Date(),
     modifiedAt: new Date()
   }, {
     id: 'other_id',
     name: 'other_name',
-    accounts: [
-      'other_account_id',
-      'another_account_id'
-    ],
     createdAt: new Date(),
     modifiedAt: new Date()
   }
