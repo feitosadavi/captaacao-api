a3920e130a06b528fb3316efe03ce29e6396d12d
feitosadavi <davifeitosa.dev@protonmail.com>
2022-03-02 18:17:17 -0300

    test: ensure Delete Mutation returns 200 on request without authentication

--- tests/main/graphql/post.test.ts
+++ tests/main/graphql/post.test.ts
@@ -1,12 +1,41 @@
 import { MongoHelper } from '@/infra/db/mongodb'
 import { setupApp } from '@/main/config/app'
 
-import { Collection } from 'mongodb'
+import { Collection, InsertOneWriteOpResult } from 'mongodb'
 import { Express } from 'express'
 import request from 'supertest'
+import { sign } from 'jsonwebtoken'
+import env from '@/main/config/env'
 
-let accountCollection: Collection
+let accountsCollection: Collection
+let postsCollection: Collection
 let app: Express
+
+const insertAccount = async (): Promise<InsertOneWriteOpResult<any>> => {
+  return accountsCollection.insertOne({
+    name: 'Teste',
+    email: 'teste@gmail.com',
+    password: '123',
+    profiles: ['admin']
+  })
+}
+
+const updateAccountToken = async (id: string, accessToken: string): Promise<void> => {
+  await accountsCollection.updateOne({
+    _id: id
+  }, {
+    $set: {
+      accessToken
+    }
+  })
+}
+
+const insertPost = async (postedBy: string): Promise<InsertOneWriteOpResult<any>> => {
+  return postsCollection.insertOne({
+    title: 'teste',
+    postedBy
+  })
+}
 
 describe('Account GraphQL', () => {
   beforeAll(async () => {
@@ -19,8 +48,10 @@ describe('Account GraphQL', () => {
   })
 
   beforeEach(async () => {
-    accountCollection = await MongoHelper.getCollection('posts')
-    await accountCollection.deleteMany({})
+    accountsCollection = await MongoHelper.getCollection('accounts')
+    await accountsCollection.deleteMany({})
+    postsCollection = await MongoHelper.getCollection('posts')
+    await postsCollection.deleteMany({})
   })
 
   describe('Delete Mutation', () => {
@@ -37,6 +68,31 @@ describe('Account GraphQL', () => {
         .post('/graphql')
         .send({ query })
       expect(res.status).toBe(403)
+    })
+
+    test('Should return 200 on request without authentication', async () => {
+      const insertAccountResult = await insertAccount()
+      const accountId = insertAccountResult.ops[0]._id
+      const accessToken = sign({ id: accountId }, env.secret)
+      await updateAccountToken(accountId, accessToken)
+
+      const insertPostResult = await insertPost(accountId)
+      const postId = insertPostResult.insertedId
+
+      const query = `mutation {
+        delete (
+          id: "${postId}"
+        ) {
+          result
+        }
+      }`
+
+      const res = await request(app)
+        .post('/graphql')
+        .set('x-access-token', accessToken)
+        .send({ query })
+      // console.log(res)
+      expect(res.status).toBe(200)
     })
   })
 })
