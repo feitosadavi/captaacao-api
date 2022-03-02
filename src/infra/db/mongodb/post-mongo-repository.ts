3239910f15833662108844b9951853965c783300
feitosadavi <davifeitosa.dev@protonmail.com>
2022-03-02 13:28:26 -0300

    feat: add DeletePostRepository

--- src/infra/db/mongodb/post-mongo-repository.ts
+++ src/infra/db/mongodb/post-mongo-repository.ts
@@ -1,9 +1,10 @@
 import { ObjectID } from 'mongodb'
 
-import { AddPostRepository, LoadPostByIdRepository, LoadAllPostsRepository } from '@/data/protocols'
+import { AddPostRepository, LoadPostByIdRepository, LoadAllPostsRepository, DeletePostRepository } from '@/data/protocols'
 import { MongoHelper } from '@/infra/db/mongodb'
+import { DeletePost } from '@/domain/usecases'
 
-export class PostMongoRepository implements AddPostRepository, LoadAllPostsRepository, LoadPostByIdRepository {
+export class PostMongoRepository implements AddPostRepository, LoadAllPostsRepository, LoadPostByIdRepository, DeletePostRepository {
   async addPost (params: AddPostRepository.Params): AddPostRepository.Result {
     const postsCollection = await MongoHelper.getCollection('posts')
     await postsCollection.insertOne(params)
@@ -19,5 +20,11 @@ export class PostMongoRepository implements AddPostRepository, LoadAllPostsRepos
     const postsCollection = await MongoHelper.getCollection('posts')
     const post = await postsCollection.findOne({ _id: new ObjectID(id) })
     return post && MongoHelper.map(post)
+  }
+
+  async deletePost ({ id }: DeletePost.Params): DeletePost.Result {
+    const postsCollection = await MongoHelper.getCollection('posts')
+    const result = await postsCollection.deleteOne({ _id: id })
+    return result.deletedCount === 1
   }
 }
