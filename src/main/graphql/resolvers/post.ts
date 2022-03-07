f429e4f4452f48389375869007d7a8d6596a2d6d
feitosadavi <davifeitosa.dev@protonmail.com>
2022-03-07 18:42:12 -0300

    feat: add AddPost graphql mutation and change delete to deletePost

--- src/main/graphql/resolvers/post.ts
+++ src/main/graphql/resolvers/post.ts
@@ -1,8 +1,10 @@
 import { adaptResolver } from '@/main/adapters'
+import { makeAddPostController } from '@/main/factories'
 import { makeDeletePostController } from '@/main/factories/controllers/post/delete-profile-controller-factory'
 
 export default {
   Mutation: {
-    delete: async (parent: any, args: any, context: any) => adaptResolver(makeDeletePostController(), args, context)
+    addPost: async (parent: any, args: any, context: any) => adaptResolver(makeAddPostController(), args, context),
+    deletePost: async (parent: any, args: any, context: any) => adaptResolver(makeDeletePostController(), args, context)
   }
 }
--- src/main/graphql/type-defs/post.ts
+++ src/main/graphql/type-defs/post.ts
@@ -2,10 +2,36 @@ import { gql } from 'apollo-server-express'
 
 export default gql`
   extend type Mutation {
-    delete (id: String!): DeletionResult! @auth
+    addPost (
+      title: String!
+      photos: [String!]
+      description: String!
+      carBeingSold: CarBeingSold
+    ): Boolean @auth
+    deletePost (id: String!): DeletionResult! @auth
   }
 
   type DeletionResult {
     result: Boolean
+  }
+
+  input CarBeingSold {
+    thumb: String!
+    price: Int!
+    fipePrice: Int!
+
+    brand: String!
+    model: String!
+    year: String!
+    color: String!
+    doors: Int!
+    steering: String!
+
+    carItems: [String]!
+    kmTraveled: Int!
+
+    licensePlate: String!
+    sold: Boolean!
+    fastSale: Boolean!
   }
 `
