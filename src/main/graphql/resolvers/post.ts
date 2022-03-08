410584b8e726028634e4e6fbabe2fda10e5689a7
feitosadavi <davifeitosa.dev@protonmail.com>
2022-03-08 20:11:30 -0300

    feat: add posts graphql query

--- src/main/graphql/resolvers/post.ts
+++ src/main/graphql/resolvers/post.ts
@@ -1,10 +1,14 @@
 import { adaptResolver } from '@/main/adapters'
-import { makeAddPostController } from '@/main/factories'
+import { makeAddPostController, makeLoadAllPostsController } from '@/main/factories'
 import { makeDeletePostController } from '@/main/factories/controllers/post/delete-profile-controller-factory'
 
 export default {
   Mutation: {
     addPost: async (parent: any, args: any, context: any) => adaptResolver(makeAddPostController(), args, context),
     deletePost: async (parent: any, args: any, context: any) => adaptResolver(makeDeletePostController(), args, context)
+  },
+
+  Query: {
+    posts: async (parent: any, args: any) => adaptResolver(makeLoadAllPostsController(), args)
   }
 }
--- src/main/graphql/type-defs/post.ts
+++ src/main/graphql/type-defs/post.ts
@@ -1,6 +1,10 @@
 import { gql } from 'apollo-server-express'
 
 export default gql`
+  extend type Query {
+    posts: [Post!]
+  }
+
   extend type Mutation {
     addPost (
       title: String!
@@ -30,6 +34,38 @@ export default gql`
     carItems: [String]!
     kmTraveled: Int!
 
+    licensePlate: String!
+    sold: Boolean!
+    fastSale: Boolean!
+  }
+
+  type Post {
+    id: String!
+    title: String!
+    photos: [String!]
+    description: String!
+
+    carBeingSold: Car
+
+    createdAt: DateTime
+    modifiedAt: DateTime
+    postedBy: String
+    status: Boolean
+    active: Boolean
+  }
+
+  type Car {
+    thumb: String!
+    price: Int!
+    fipePrice: Int!
+    brand: String!
+    model: String!
+    year: String!
+    color: String!
+    doors: Int!
+    steering: String!
+    carItems: [String]!
+    kmTraveled: Int!
     licensePlate: String!
     sold: Boolean!
     fastSale: Boolean!
