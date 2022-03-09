1ecee86dec787b75c15210ac25dbf1ed96acf27d
feitosadavi <davifeitosa.dev@protonmail.com>
2022-03-09 19:42:31 -0300

    chore: add cors middleware

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/require-await */
 import express, { Express } from 'express'
 import { setupApolloServer } from '../graphql/apollo'
+import { cors } from '../middlewares'
 
 export const setupApp = async (): Promise<Express> => {
   const app = express()
+  app.use(cors)
   const server = setupApolloServer()
   await server.start()
   server.applyMiddleware({ app })
