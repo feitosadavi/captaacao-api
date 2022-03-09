e39c12b44b60b03cf3b9c13d9e0642096b72a329
feitosadavi <davifeitosa.dev@protonmail.com>
2022-03-09 16:47:35 -0300

    refactor: get nodemailer tests back

---
+++ tests/infra/messaging/nodemailer-adapter.test.ts
@@ -0,0 +1,40 @@
+import { SendEmailRepository, SetupEmailRepository } from '@/data/protocols/messaging'
+import { NodemailerAdapter } from '@/infra/messaging'
+import env from '@/main/config/env'
+
+type SutType = {
+  sut: NodemailerAdapter
+}
+
+const makeSut = (): SutType => {
+  const sut = new NodemailerAdapter()
+  return { sut }
+}
+
+const mockEmailParams = (): SendEmailRepository.Params => ({
+  from: `"any_name" <${env.testingEmail1}>`,
+  to: env.testingEmail2,
+  subject: 'Recuperação da Senha - Captação',
+  text: 'Aqui está o código para a recuperação da sua senha: 123'
+})
+
+const mockSetupParams = (): SetupEmailRepository.Params => ({
+  service: 'gmail',
+  user: env.testingEmail1,
+  pass: env.testingEmailPassword1
+})
+
+describe('NodemailerAdapter', () => {
+  test('Should return false if setup was not executed', async () => {
+    const { sut } = makeSut()
+    const result = await sut.send(mockEmailParams())
+    expect(result).toBe(false)
+  })
+
+  test('Should return true if email was sent correctly', async () => {
+    const { sut } = makeSut()
+    sut.setup(mockSetupParams())
+    const result = await sut.send(mockEmailParams())
+    expect(result).toBe(true)
+  })
+})
