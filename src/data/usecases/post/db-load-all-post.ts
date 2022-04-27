c53754c1bc2e093fec9463cc676c80769515ed39
feitosadavi <davifeitosa.dev@protonmail.com>
2022-04-26 22:11:59 -0300

    feat: ensure DbLoadAllPosts calls LoadAllPostsRepository with correct filters, if it exists

--- src/data/usecases/post/db-load-all-post.ts
+++ src/data/usecases/post/db-load-all-post.ts
@@ -4,8 +4,8 @@ import { LoadAllPostsRepository } from '@/data/protocols'
 export class DbLoadAllPosts implements LoadAllPosts {
   constructor (private readonly loadAllPostsRepository: LoadAllPostsRepository) { }
 
-  async load (): LoadAllPosts.Result {
-    const posts = await this.loadAllPostsRepository.loadAll()
+  async load (params: LoadAllPosts.Params): LoadAllPosts.Result {
+    const posts = await this.loadAllPostsRepository.loadAll(params)
     return posts
   }
 }
