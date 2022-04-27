95a6b05823d9278749de2b29b1902157acef8a98
feitosadavi <davifeitosa.dev@protonmail.com>
2022-04-27 17:25:03 -0300

    feat: add postedBy filter to posts query

--- src/main/graphql/type-defs/post.ts
+++ src/main/graphql/type-defs/post.ts
@@ -2,7 +2,7 @@ import { gql } from 'apollo-server-express'
 
 export default gql`
   extend type Query {
-    posts: [Post!]
+    posts(postedBy: String): [Post!]
     post (id: String!): Post
   }
 
