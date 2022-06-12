f14c427c6b43db79df8f2fe5c7346dd55ba49724
feitosadavi <davifeitosa.dev@protonmail.com>
2022-06-12 17:51:24 -0300

    refactor: change update-post rote structure

--- src/main/routes/post.routes.ts
+++ src/main/routes/post.routes.ts
@@ -1,10 +1,11 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import { adaptRoute, adaptMiddleware } from '@/main/adapters'
-import { makeAddPostController, makeAuthMiddleware } from '@/main/factories'
+import { makeAddPostController, makeUpdatePostController, makeAuthMiddleware } from '@/main/factories'
 import { multer } from '../middlewares'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/post', multer('photos'), adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeAddPostController()))
+  router.post('/update-post/:id', multer('photos'), adaptMiddleware(makeAuthMiddleware()), adaptRoute(makeUpdatePostController()))
 }
