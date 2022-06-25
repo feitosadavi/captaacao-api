d9949b1b3277452a7732b245ddbbe2808e30bb3c
feitosadavi <davifeitosa.dev@protonmail.com>
2022-06-25 09:25:38 -0300

    feat: add lookup to favouritesList field on loadById

--- src/infra/db/mongodb/account-mongo-repository.ts
+++ src/infra/db/mongodb/account-mongo-repository.ts
@@ -58,8 +58,23 @@ export class AccountMongoRepository implements
 
   async loadById ({ id }: LoadAccountByIdRepository.Params): LoadAccountByIdRepository.Result {
     const accountsCollection = await MongoHelper.getCollection('accounts')
-    const account = await accountsCollection.findOne({ _id: new ObjectID(id) })
-    return account && MongoHelper.map(account)
+    const accountsNonMappedId = await accountsCollection.aggregate([
+      {
+        $match: { $expr: { $eq: ['$_id', new ObjectID(id)] } }
+      },
+      {
+        $lookup: {
+          from: 'posts',
+          localField: 'favouritesList',
+          foreignField: '_id',
+          as: 'favouritesList'
+        }
+      }
+    ]).toArray()
+    if (accountsNonMappedId[0]?.favouritesList.length > 0) {
+      accountsNonMappedId[0].favouritesList = MongoHelper.mapCollection(accountsNonMappedId[0].favouritesList)
+    }
+    return accountsNonMappedId[0] && MongoHelper.map(accountsNonMappedId[0])
   }
 
   async loadByEmail ({ email }: LoadAccountByEmailRepository.Params): LoadAccountByEmailRepository.Result {
