1f2b8048abeeaad91e53f0de3fe96b3e9a7d61e6
feitosadavi <davifeitosa.dev@protonmail.com>
2022-07-19 17:00:11 -0300

    chore: config subscriptions

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,18 +1,27 @@
 /* eslint-disable @typescript-eslint/require-await */
-import express, { Express } from 'express'
+import express from 'express'
 import { setupApolloServer } from '../graphql/apollo'
 import setupRoutes from './routes'
 import cors from 'cors'
+import { createServer, Server } from 'http'
+import path from 'path'
 
-export const setupApp = async (): Promise<Express> => {
+export const setupApp = async (): Promise<Server> => {
   const app = express()
   // app.use(express.json())
   // app.use(express.urlencoded({ extended: true }))
   app.use(cors({ origin: '*' }))
 
   setupRoutes(app)
-  const server = setupApolloServer()
+
+  const httpServer = createServer(app)
+
+  app.get('/graphql', (req, res) => {
+    res.sendFile(path.join(__dirname, '../static/graphiql-over-ws.html'))
+  })
+  const server = setupApolloServer(httpServer)
   await server.start()
   server.applyMiddleware({ app })
-  return app
+
+  return httpServer
 }
--- src/main/graphql/apollo/apollo-server.ts
+++ src/main/graphql/apollo/apollo-server.ts
@@ -1,12 +1,19 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable @typescript-eslint/require-await */
+import { WebSocketServer } from 'ws'
+import { useServer } from 'graphql-ws/lib/use/ws'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { ApolloServer } from 'apollo-server-express'
+import { GraphQLError, print } from 'graphql'
+import { Server } from 'http'
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
+import { PubSub } from 'graphql-subscriptions'
+import { GraphQLLiveDirective } from '@n1ru4l/graphql-live-query'
+
 import typeDefs from '@/main/graphql/type-defs'
 import resolvers from '@/main/graphql/resolvers'
 import { authDirectiveTransformer } from '@/main/graphql/directives'
-
-import { makeExecutableSchema } from '@graphql-tools/schema'
-import { ApolloServer } from 'apollo-server-express'
-import { GraphQLError } from 'graphql'
+import { astFromDirective } from '@graphql-tools/utils'
 
 const handleErrors = (response: any, errors: readonly GraphQLError[]): void => {
   errors?.forEach(error => {
@@ -27,15 +34,35 @@ const checkError = (error: GraphQLError, errorName: string): boolean => {
   return [error.name, error.originalError?.name].some(name => name === errorName)
 }
 
-let schema = makeExecutableSchema({ resolvers, typeDefs })
+const liveDirectiveTypeDefs = print(
+  // @ts-ignore: Unreachable code error
+  astFromDirective(GraphQLLiveDirective)
+)
+
+let schema = makeExecutableSchema({ resolvers, typeDefs: [...typeDefs, liveDirectiveTypeDefs] })
 schema = authDirectiveTransformer(schema)
 
-export const setupApolloServer = (): ApolloServer => new ApolloServer({
-  schema,
-  context: ({ req }) => ({ req }),
-  plugins: [{
-    requestDidStart: async () => ({
-      willSendResponse: async ({ response, errors }) => handleErrors(response, errors)
-    })
-  }]
-})
+export const setupApolloServer = (httpServer: Server): ApolloServer => {
+  const wsServer = new WebSocketServer({
+    path: '/graphql',
+    server: httpServer
+  })
+
+  const serverCleanup = useServer({ schema }, wsServer)
+
+  const pubsub = new PubSub()
+  return new ApolloServer({
+    schema,
+    context: ({ req }) => ({ req, pubsub }),
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        serverWillStart: async () => ({
+          drainServer: async () => await serverCleanup.dispose()
+        }),
+        requestDidStart: async () => ({
+          willSendResponse: async ({ response, errors }) => handleErrors(response, errors)
+        })
+      }]
+  })
+}
